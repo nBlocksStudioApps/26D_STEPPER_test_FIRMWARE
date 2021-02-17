5520481fea529a1875ceff3c15fd0588fd802a31
nikos chalikias <nikos.chalikias@gmail.com>
2021-02-17 11:45:53 +0000

    images and readme

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 // -*-*- List of node objects -*-*-
 nBlock_Ticker            nb_nBlockNode0_Ticker       (1000);
-nBlock_Counter           nb_nBlockNode1_Counter      (2);
+nBlock_Counter           nb_nBlockNode1_Counter      (5);
 nBlock_STEPPER           nb_nBlockNode2_STEPPER      (p11, p12, p13, P0_11, p26, p29, p30, p25, 0.00004, 10, 1, true);
 nBlock_StringFormat      nb_nBlockNode3_StringFormat ("stop at %d\r");
 nBlock_OLEDisplay        nb_nBlockNode4_OLEDisplay   (p9, p10, p27);
